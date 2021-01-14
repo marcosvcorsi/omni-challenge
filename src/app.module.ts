148882ed32df0b6924d229c61e1af0315991fbc6
Marcos Corsi <marcosvcorsi@gmail.com>
2021-01-14 20:10:41 -0300

    feat: add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
