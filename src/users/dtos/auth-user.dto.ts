840a13ca522b35789f08711908b6ee4d96c68fa5
Marcos Corsi <marcosvcorsi@gmail.com>
2021-01-14 22:13:55 -0300

    feat: add validator to dtos

--- src/users/dtos/auth-user.dto.ts
+++ src/users/dtos/auth-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
 export class AuthUserDto {
+  @IsString()
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(6)
   password: string;
 }
--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
   name: string;
+
+  @IsString()
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(6)
   password: string;
 }
